f5a3f0d0514a9055810ba336bc50e3f3e0b97290
Carlosfidelrosales <fidelcarlos302@gmail.com>
2023-01-14 15:35:19 +0800

    Added the build tree that will store the elements for my full name. Also added the letters of my full name that will be tested.

--- BinaryTreeFullName.py
+++ BinaryTreeFullName.py
@@ -100,3 +100,15 @@ class BinarySearchTreeNode:
             self.left = self.left.delete(max_val)
 
         return self
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    alphabets = ["C", "A", "R", "L", "O", "S", "F", "I", "D", "E", "L", "R", "O", "S", "A", "L", "E", "S"]
+    alphabets_tree = build_tree(alphabets)
