48cedd97e1db3f6d72eb420fa2cd189ab5b67e55
Carlosfidelrosales <fidelcarlos302@gmail.com>
2023-01-14 10:59:10 +0800

    Added a def function to add a child node to the tree.

--- BTcodePart1.py
+++ BTcodePart1.py
@@ -4,3 +4,18 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
+    def add_child(self, data):
+            if data == self.data:
+                return # node already exist
+
+            if data < self.data: #add data in the left subtree  
+                if self.left:
+                    self.left.add_child(data)
+                else:
+                    self.left = BinarySearchTreeNode(data)
+            else: #add data in the right subtree
+                if self.right:
+                    self.right.add_child(data)
+                else:
+                    self.right = BinarySearchTreeNode(data)
+
