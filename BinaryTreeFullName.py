b3bce7eecd1d265673c5245c874dcdfc1fecbb9e
Carlosfidelrosales <fidelcarlos302@gmail.com>
2023-01-14 15:26:02 +0800

    Added the remaining def functions that will support for the utilization of the program (search, obtaining min and max and delete).

--- BinaryTreeFullName.py
+++ BinaryTreeFullName.py
@@ -51,3 +51,52 @@ class BinarySearchTreeNode:
             elements += self.left.preOrder_traversal()
         if self.right:
             elements += self.right.preOrder_traversal()
+
+        return elements
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+    
+    def obt_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.obt_max()
+
+    def obt_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.obt_min()
+
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.right
+
+            max_val = self.left.obt_max()
+            self.data = max_val
+            self.left = self.left.delete(max_val)
+
+        return self
