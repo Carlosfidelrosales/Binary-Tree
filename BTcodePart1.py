6703a93f5af181b369334e534c6fcb2acf444d8b
Carlosfidelrosales <fidelcarlos302@gmail.com>
2023-01-14 11:15:32 +0800

    Added the search function that let the program find a specific country on a given tree.

--- BTcodePart1.py
+++ BTcodePart1.py
@@ -34,6 +34,22 @@ class BinarySearchTreeNode:
                 elements += self.right.in_order_traversal()
 
             return elements
+
+    def search(self, val):
+            if self.data == val:
+                return True
+
+            if val < self.data:
+                if self.left:
+                    return self.left.search(val)
+                else:
+                    return False
+
+            if val > self.data:
+                if self.right:
+                    return self.right.search(val)
+                else:
+                    return False
     
 def build_tree(elements):
         print("\nBuilding tree with these elements:",elements)
@@ -47,3 +63,11 @@ def build_tree(elements):
 if __name__ == '__main__':
         numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
         print("In order traversal gives this sorted list (NUMBERS) :",numbers_tree.in_order_traversal())
+
+        countries = ["India","Pakistan","Germany", "USA","China","India","UK","USA"]
+        country_tree = build_tree(countries)
+        print("In order traversal gives this sorted list (COUNTRIES) :",country_tree.in_order_traversal())
+
+        print("\nSEARCH METHOD ON BUILD TREE")
+        print("Is Philippines in the list? ", country_tree.search("Philippines"))
+        print("Is Pakistan is in the list? ", country_tree.search("Pakistan"))
