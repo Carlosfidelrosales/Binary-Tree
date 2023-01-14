8ebaa322575bfa04b0224b099d62ad5f9299ba38
Carlosfidelrosales <fidelcarlos302@gmail.com>
2023-01-14 13:02:18 +0800

    Added the def function for the three traversals in order, pre order and post order.

--- BTcodePart1EXERCISE.py
+++ BTcodePart1EXERCISE.py
@@ -3,8 +3,8 @@
 # 1. find_min(): finds minimum element in entire binary tree
 # 2. find_max(): finds maximum element in entire binary tree
 # 3. calculate_sum(): calcualtes sum of all elements
-# 4. post_order_traversal(): performs post order traversal of a binary tree
-# 5. pre_order_traversal(): perofrms pre order traversal of a binary tree
+# 4. inOrder_traversal(): performs post order traversal of a binary tree
+# 5. preOrder_traversal(): perofrms pre order traversal of a binary tree
     
 class BinarySearchTreeNode:
     def __init__(self, data):
@@ -26,6 +26,38 @@ class BinarySearchTreeNode:
                 self.right.join_child(data)
             else:
                 self.right = BinarySearchTreeNode(data)
+
+    def inOrder_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.inOrder_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.inOrder_traversal()
+
+        return elements
+
+    def postOrder_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.postOrder_traversal()
+        if self.right:
+            elements += self.right.postOrder_traversal()
+
+        elements.append(self.data)
+
+        return elements
+
+    def preOrder_traversal(self):
+        elements = [self.data]
+        if self.left:
+            elements += self.left.preOrder_traversal()
+        if self.right:
+            elements += self.right.preOrder_traversal()
+
+        return elements
     
 
 
